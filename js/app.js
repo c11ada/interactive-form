58fe1dbca7cfdfd188f70d1c8b63b939d0d09dd6
Muhammad Nadat <muhammad.nadat@gmail.com>
2017-09-10 17:59:46 +0400

    ”Register for Activities” section of the form
    New style added
    index.html markup changed
    app.js logic added for activites

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const otherTitleInput = document.querySelector("#other-title");
     const themeSelect = document.querySelector("#design");
     const colorsDiv = document.querySelector("#colors-js-puns");
+    const activityChk = document.querySelectorAll('[type="checkbox"]');
+    let confTotal = 0;
+
+    // <label>Your Total is $ <span>0</span></label>
+    const totalDiv = document.createElement("label");
+    totalDiv.id = "totalDiv";
+    totalDiv.textContent = "Your Total is $ ";
+    const totalSpan = document.createElement("span");
+    totalSpan.id = "totalSpan";
+    totalSpan.textContent= confTotal;
+    totalDiv.appendChild(totalSpan);
+    document.querySelector(".activities").appendChild(totalDiv);
 
     const colorOption = {
         'js puns' : [
@@ -58,6 +70,42 @@ document.addEventListener("DOMContentLoaded", () => {
         } else  {
             colorsDiv.style.display = "none";
         }
+    });
+
+    activityChk.forEach(function(element) {
+        element.addEventListener("change", (e) => {
+            const price = e.target.getAttribute("data-price");
+            const activityName = e.target.getAttribute("name");
+            const isChecked = e.target.checked;
+
+            if (isChecked) {
+                totalSpan.textContent = confTotal + price;
+            } else {
+                totalSpan.textContent = confTotal - price;
+            }
+
+            switch (activityName) {
+                case "js-frameworks":  
+                    document.querySelector('[name="express"]').disabled = isChecked;
+                    document.querySelector('[name="express"]').parentNode.className = "disabled-" + isChecked;
+                    break;
+                case "js-libs":
+                    document.querySelector('[name="node"]').disabled = isChecked;
+                    document.querySelector('[name="node"]').parentNode.className = "disabled-" + isChecked;
+                    break;    
+                case "node":
+                    document.querySelector('[name="js-libs"]').disabled = isChecked;
+                    document.querySelector('[name="js-libs"]').parentNode.className = "disabled-" + isChecked;
+                    break;
+                case "express":
+                    document.querySelector('[name="js-frameworks"]').disabled = isChecked;
+                    document.querySelector('[name="js-frameworks"]').parentNode.className = "disabled-" + isChecked;
+                    break;
+                default:
+                    console.log("something else pressed pressed");
+                    break;
+            }
+        });  
     });
 
 });
